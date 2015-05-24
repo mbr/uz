9bef029e49c6055358efdb25f736bbbbaa9b3baa
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-24 17:04:19 +0200

    Initial import.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='uz',
+    version='0.1dev',
+    description='uz calls the correct program to extract packages',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/uz',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['click'],
+    entry_points={
+        'console_scripts': [
+            'uz = uz.cli:uz',
+        ],
+    }
+)
---
+++ uz/cli.py
@@ -0,0 +1,6 @@
+import click
+
+
+@click.command
+def uz():
+    click.echo('uzuzuz')
