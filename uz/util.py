8db731bee74a6532ffc9fe3e19ba8d396bef6fba
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-25 11:17:56 +0200

    Added StringIO-on-steroids.

--- uz/util.py
+++ uz/util.py
@@ -1,3 +1,68 @@
+class ReadView(object):
+    def __init__(self, buf, offset=0, limit=None):
+        if not isinstance(offset, int) or not isinstance(limit, int):
+            raise ValueError('an integer is required')
+
+        if limit < 0 or offset < 0:
+            raise ValueError('value must be positive')
+
+        if offset >= len(buf) or limit > len(buf):
+            raise ValueError('value out of range')
+
+        if limit < offset:
+            raise ValueError('limit must be >= offset')
+
+        self.limit = limit
+        self.offset = offset
+        self.buf = buf
+        self.pos = offset
+
+    def _get_pos(self):
+        return self.pos + self.offset
+
+    def __len__(self):
+        return self.limit - self.offset
+
+    def read(self, n=-1):
+        if not isinstance(n, int):
+            raise TypeError('an integer is required')
+
+        buflen = len(self)
+        pos = self._get_pos()
+        maxread = buflen - pos
+        newpos = self.pos + n
+
+        self.pos += n
+        return self.buf[pos:pos+n]
+
+    def tell(self):
+        return self._get_pos()
+
+    def seek(self, offset, whence=0):
+        if not isinstance(offset, int) or not isinstance(whence, int):
+            raise TypeError('an integer is required')
+
+        if whence == 0 and not offset > 0:
+            raise ValueError('offset must be >= 0')
+
+        if whence == 0:
+            newpos = offset
+        elif whence == 1:
+            newpos = self.pos + offset
+        elif whence == 2:
+            newpos = len(self.buf) + offset
+        else:
+            raise ValueError('invalid value for whence')
+
+        if newpos < 0 or newpos >= len(self.buf):
+            raise ValueError('invalid offset')
+
+        self.pos = newpos
+
+    def seekable(self):
+        return True
+
+
 class RandomAccessBuffer(object):
     def __init__(self, reader):
         self.reader = reader
